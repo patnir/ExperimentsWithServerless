9328018e3b540eaba3d6a23409cda3cbfe729f48
Rahul Patni <rpatni@purdue.edu>
2018-08-01 21:16:16 +0530

    going to make a grocery counter app

--- learningReact/first-app/react-app/src/index.js
+++ learningReact/first-app/react-app/src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import Counter from "../src/components/counter";
+const element = <h1>Hello World!</h1>;
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<Counter />, document.getElementById("root"));
+ReactDOM.render(element, document.getElementById("root1"));
