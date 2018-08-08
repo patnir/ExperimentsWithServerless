34c7f6fc4fe628d2a511e214b01c4fa7314ab7e9
Rahul Patni <rpatni@purdue.edu>
2018-08-08 11:12:20 +0530

    create api endpoint

---
+++ classGroup/classGroup-app-api/createGroup.js
@@ -0,0 +1,34 @@
+import uuid from "uuid";
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+export async function main(event, context, callback) {
+  const data = JSON.parse(event.body);
+
+  const params = {
+    TableName: "classGroups",
+    Item: {
+      classGroupId: uuid.v1(),
+      userId: event.requestContext.identity.cognitoIdentityId,
+      location: data.location,
+      notes: data.notes,
+      meetingTime: data.meetingTime,
+      content: data.content,
+      attendanceLimit: data.attendanceLimit,
+      participantIds: data.participantIds,
+      commentIds: data.commentIds,
+      attachment: data.attachment,
+      createdAt: Date.now(),
+      updatedAt: Date.now()
+    }
+  };
+
+  try {
+    await dynamoDbLib.call("put", params);
+    callback(null, success(params.Item));
+  } catch (e) {
+    console.log(e);
+
+    callback(null, failure({ status: false }));
+  }
+}
